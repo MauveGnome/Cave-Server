527ccfdcdabd8beec6365bdb796d58d0e4936dbe
Sam <Sam@Sam-PC.home>
2013-03-27 00:01:22 +0000

    Tweaks

--- src/server/Vote.java
+++ src/server/Vote.java
@@ -1,9 +1,22 @@
 package server;
 
+import java.util.Set;
+
 /**
+ * Vote.class
+ * 
+ * Represents a single issue to be voted on.
+ * 
  * @author Sam Beed B0632953
  */
 
 public class Vote {
+    
+    String question;
+    Set<String> answers;
+    
+    public Vote(String newQuestion) {
+        
+    }
     
 }
